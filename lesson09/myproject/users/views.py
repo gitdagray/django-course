f385c343ef95733e6b93a291b9b12fdb51d50d90
gitdagray <DaveOnEleven@gmail.com>
2024-02-25 12:34:24 -0600

    Added lesson09

--- lesson09/myproject/users/views.py
+++ lesson09/myproject/users/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, redirect 
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm 
 
 # Create your views here.
 def register_view(request):
-    if request.method == 'POST': 
-        form = UserCreationForm(request.POST)
+    if request.method == "POST": 
+        form = UserCreationForm(request.POST) 
         if form.is_valid(): 
             form.save() 
             return redirect("posts:list")
